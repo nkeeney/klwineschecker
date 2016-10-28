d846c72b4b6798905149ee602c79a6721ae4feff
nkeeney <nkeeney@gmail.com>
2016-10-28 16:00:58 -0700

    Fix DB query

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/dbsetup', function (request, response) {
 
 app.get('/dbinit', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('INSERT INTO result_table VALUES (1, "blank")', function(err, result) {
+    client.query("INSERT INTO result_table (id, html) VALUES (1, 'blank')", function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
