c3fd5bde4ac9ee6bd482e1c708ac47730c7615b2
nkeeney <nkeeney@gmail.com>
2016-10-28 14:54:58 -0700

    Bugfix for stripping.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', function(request, response) {
 		// str is a bit batch of HTML.
 		
 		// strip everything before <div class="results-block clearfix"> until <div class="page-filters-block clearfix">
-		str = str.subtring(str.indexOf("<div class=\"results-block clearfix\">"), str.indexOf("<div class=\"page-filters-block clearfix\">"));
+		str = str.subtring(str.indexOf("results-block clearfix"), str.indexOf("page-filters-block clearfix"));
 		
 		response.send(str);
 	  });
