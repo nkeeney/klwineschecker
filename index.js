12042e6737f24184c753b6ba03eaca5afe87103a
nkeeney <nkeeney@gmail.com>
2016-10-28 15:36:06 -0700

    Installing PostGREs

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 var express = require('express')
 var app = express()
 var http = require('http');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('create table result_table (id integer, html text)', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM result_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 var options = {
   host: 'www.klwines.com',
